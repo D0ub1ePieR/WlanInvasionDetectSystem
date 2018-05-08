3b4c2aeb9c236f227ba99eba64cb0d876dabde07
tangtangsama <1462339680@qq.com>
2018-05-08 20:55:08 +0800

    完善GetOnlineWhiteListWifiList类

--- src/main/java/cn/nuaa/spicydick/server/handler/wifiDetection/GetOnlineWhiteListWifiList.java
+++ src/main/java/cn/nuaa/spicydick/server/handler/wifiDetection/GetOnlineWhiteListWifiList.java
@@ -1,6 +1,7 @@
 package cn.nuaa.spicydick.server.handler.wifiDetection;
 
 import cn.nuaa.spicydick.server.RequestHandler;
+import cn.nuaa.spicydick.server.handler.usr.Login;
 import cn.nuaa.spicydick.server.handler.wifiDetection.Info.OnlineWhiteListWifiInfo;
 import cn.nuaa.spicydick.server.msg.Request;
 import cn.nuaa.spicydick.server.msg.ErrorCode;
@@ -8,8 +9,13 @@ import cn.nuaa.spicydick.server.msg.ResponseFactory;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
 import org.apache.log4j.Logger;
+//import za.co.towerman.jkismet.message.InfoMessage;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import static cn.nuaa.spicydick.server.RegExprFix.formDetect;
+import static cn.nuaa.spicydick.server.Tools.StringToInt;
 
 //返回当前在线且在白名单的热点列表
 public class GetOnlineWhiteListWifiList extends RequestHandler {
@@ -19,8 +25,46 @@ public class GetOnlineWhiteListWifiList extends RequestHandler {
     public void handle(RoutingContext routingContext, final Request request) {
         routingContext.response().putHeader("content-type", "application/json");
 
-        //int page = request.getParams().getValue("page") != null ?         //是否分页
-                //StringToInt(request.getParams().getValue("page").toString()) : 1;
+        //是否分页,1表示不分页
+        int page = request.getParams().getValue("page") != null ? StringToInt(request.getParams().getValue("page").toString()) : 1;
+
+        //表单验证
+        if(!formDetect(page, 1, 100)){
+            routingContext.response().end(ResponseFactory.error(-3, ErrorCode.INVALID_PARAMETERS, "非法参数").toString());
+            Login.logger.error((Object)String.format("user:%s exception:%s", "非法参数"));
+            return;
+        }
+
+        JsonObject result = new JsonObject();
+        List<OnlineWhiteListWifiInfo> onlineWhiteListWifiInfoList = new ArrayList<OnlineWhiteListWifiInfo>();
+
+        OnlineWhiteListWifiInfo Info1 = new OnlineWhiteListWifiInfo();
+        Info1.setWifiSignal("66");
+        Info1.setWifiSsid("TPLINK1");
+        Info1.setCompany("huawei");
+        Info1.setSecurityStatus("0");//0表示在内网不在白名单中，不安全
+        Info1.setWifiBssid("FF-FF-FF-FF-FF-FF");
+
+        OnlineWhiteListWifiInfo Info2 = new OnlineWhiteListWifiInfo();
+        Info2.setWifiSignal("66");
+        Info2.setWifiSsid("TPLINK2");
+        Info2.setCompany("huawei");
+        Info2.setSecurityStatus("1");//1表示在内网且在白名单中，安全
+        Info2.setWifiBssid("FF-FF-FF-FF-FF-FF");
+
+        OnlineWhiteListWifiInfo Info3 = new OnlineWhiteListWifiInfo();
+        Info3.setWifiSignal("66");
+        Info3.setWifiSsid("TPLINK#");
+        Info3.setCompany("huawei");
+        Info3.setSecurityStatus("2");//2表示不在内网也不在白名单，外部wifi可忽略
+        Info3.setWifiBssid("FF-FF-FF-FF-FF-FF");
+
+        onlineWhiteListWifiInfoList.add(Info1);
+        onlineWhiteListWifiInfoList.add(Info2);
+        onlineWhiteListWifiInfoList.add(Info3);
+
+        result.put("onlineWhiteListWifiInfoList", onlineWhiteListWifiInfoList);
+        routingContext.response().end(ResponseFactory.success(request, result).toString());
 
     }
 }
