85bc9184cf55fbaee7ed1c06d9391c9692e4b08c
tangtangsama <1462339680@qq.com>
2018-05-08 20:50:02 +0800

    注释未使用的类

--- src/main/java/cn/nuaa/spicydick/server/handler/wifiDetection/GetStatisticsInfo.java
+++ src/main/java/cn/nuaa/spicydick/server/handler/wifiDetection/GetStatisticsInfo.java
@@ -5,9 +5,10 @@ import cn.nuaa.spicydick.server.RequestHandler;
 //import cn.nuaa.spicydick.server.Tools;
 import cn.nuaa.spicydick.server.msg.Request;
 import cn.nuaa.spicydick.server.msg.ResponseFactory;
-import cn.nuaa.spicydick.server.msg.ErrorCode;
+//import cn.nuaa.spicydick.server.msg.ErrorCode;
 import io.vertx.core.json.JsonObject;
-import io.vertx.ext.mongo.MongoClient;
+//import io.vertx.ext.mongo.MongoClient;
+//import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.RoutingContext;
 import org.apache.log4j.Logger;
 
@@ -58,15 +59,10 @@ public class GetStatisticsInfo extends RequestHandler {
 
 
         //    context.Response.End()的用法和本质：
-        //
         //    用法：可以用来终止进程，即当前HttpHandler的执行,
-        //
         //　　也可以在子方法中终止HttpHandler的执行，
-        //
         //　　实际在子方法中终止程序，只有一种可能，那就是程序抛异常，所以context.Response.End()得本质就是抛出了线程异常
-        //
         //　　由于抛异常的效率较低，所以能不能就不用，在主方法中最好还是使用return;终止程序比较好，
-        //
         //　　但是在子方法中只能使用抛异常终止程序，即使用context.Response.End()终止程序。
         routingContext.response().end(ResponseFactory.success(request,result).toString());
     }
