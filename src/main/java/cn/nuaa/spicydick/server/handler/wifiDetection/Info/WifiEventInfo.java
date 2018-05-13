96e4b48075d540defccb50e1e28a7c67afe559c1
tangtangsama <1462339680@qq.com>
2018-05-13 13:58:46 +0800

    修改WIfiEventInfo中错误的变量名

--- src/main/java/cn/nuaa/spicydick/server/handler/wifiDetection/Info/WifiEventInfo.java
+++ src/main/java/cn/nuaa/spicydick/server/handler/wifiDetection/Info/WifiEventInfo.java
@@ -1,10 +1,10 @@
 package cn.nuaa.spicydick.server.handler.wifiDetection.Info;
 
 public class WifiEventInfo {
-    public String eventType;        //时间类型
-    public String lastOccurTime;    //最后出现时间
-    public String firstOccurTime;   //最早出现时间
-    public String eventContent;     //事件目录
+    public String eventType;        //事件类型
+    public String lastOccurTime;    //最近发生时间
+    public String firstOccurTime;   //最早发生时间
+    public String eventContent;     //事件内容
     public String relatedNetworkName;//相关网络名称
     public String relatedNetworkAddress;//相关网络地址
     public String sourceInfo;       //源信息
@@ -18,7 +18,7 @@ public class WifiEventInfo {
     public String getRelatedNetworkName() { return relatedNetworkName; }
     public String getRelatedNetworkAddress() { return relatedNetworkAddress; }
     public String getSourceInfo() { return sourceInfo; }
-    public String getTargetinfo() { return targetInfo; }
+    public String getTargetInfo() { return targetInfo; }
 
     public void setEventType(String eventType) { this.eventType = eventType; }
     public void setLastOccurTime(String lastOccurTime) { this.lastOccurTime = lastOccurTime; }
@@ -27,6 +27,6 @@ public class WifiEventInfo {
     public void setRelatedNetworkName(String relatedNetworkName) { this.relatedNetworkName = relatedNetworkName; }
     public void setRelatedNetworkAddress(String relatedNetworkAddress) { this.relatedNetworkAddress = relatedNetworkAddress; }
     public void setSourceInfo(String sourceInfo) { this.sourceInfo = sourceInfo; }
-    public void setTargetinfo(String targetinfo) { this.targetInfo = targetinfo; }
+    public void setTargetInfo(String targetInfo) { this.targetInfo = targetInfo; }
 
 }
