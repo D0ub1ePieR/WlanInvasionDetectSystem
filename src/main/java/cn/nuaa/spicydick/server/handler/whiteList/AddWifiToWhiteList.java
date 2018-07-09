3504a2bc1c9f730a27d0d0845b5026b1067ad384
tangtangsama <1462339680@qq.com>
2018-07-09 13:16:30 +0800

    完善AddWifiToWhiteList()类

--- src/main/java/cn/nuaa/spicydick/server/handler/whiteList/AddWifiToWhiteList.java
+++ src/main/java/cn/nuaa/spicydick/server/handler/whiteList/AddWifiToWhiteList.java
@@ -28,7 +28,9 @@ public class AddWifiToWhiteList extends RequestHandler {
         List<ClientInfo> clientInfoList = new ArrayList<ClientInfo>();
         try {
             Gson gson = new Gson();
+            // json接受request返回值
             String json = request.getParams().getValue("clientInfoList").toString();
+            // fromJson实现从json相关对象到JAVA实体的转换，TypeToken是gson提供的数据类型转换器
             clientInfoList = gson.fromJson(json, new TypeToken<List<ClientInfo>>() {
             }.getType());
         } catch (Exception e) {
@@ -37,11 +39,12 @@ public class AddWifiToWhiteList extends RequestHandler {
             cn.nuaa.spicydick.server.handler.usr.Login.logger
                     .error((Object) String.format("user:%s exception:%s", "非法参数"));
             return;
-
         }
+
         JsonObject result = new JsonObject();
         List<singleResult> addWifiToWhiteListResultList = new ArrayList<singleResult>();
 
+        // 将客户端信息使用迭代器输出
         for (Iterator it = clientInfoList.iterator(); it.hasNext(); ) {
             ClientInfo clientInfo = (ClientInfo) it.next();
 
@@ -61,8 +64,7 @@ public class AddWifiToWhiteList extends RequestHandler {
                 return;
             }
 
-            //
-
+            // 设置单条记录信息
             singleResult Info = new singleResult();
             Info.setSingCode(0);
             Info.setSingleMessage("aa");
