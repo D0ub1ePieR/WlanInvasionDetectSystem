06a4dc8cda80ba1f29bf499d9b7815ed4a56d1b6
D0ub1ePieR <535447508@qq.com>
2018-07-22 22:08:06 +0800

    config TODO添加正则匹配

--- src/main/java/cn/nuaa/spicydick/server/RegExprFix.java
+++ src/main/java/cn/nuaa/spicydick/server/RegExprFix.java
@@ -9,15 +9,18 @@ public class RegExprFix
     //TODO 添加正则匹配
     private static final HashMap <String, String> RegExprMap = new HashMap<String, String>()
     {{
-        put("URL_RegExpr","");
-        put("MAC_RegExpr","");
-        put("INT_RegExpr","");
-        put("KeyWord_RegExpr","");
+        //地址匹配 (http(s))
+        put("URL_RegExpr","^http[s]?:\\\\/\\\\/([\\\\w-]+\\\\.)+[\\\\w-]+([\\\\w-./?%&=]*)?$");
+        //mac地址匹配
+        put("MAC_RegExpr","^([0-9A-Fa-f]{2}-){5}[0-9A-Fa-f]{2}$");
+        put("DATA_RegExpr","^((((1[6-9]|[2-9]\\\\d)\\\\d{2})-(0?[13578]|1[02])-(0?[1-9]|[12]\\\\d|3[01]))|(((1[6-9]|[2-9]\\\\d)\\\\d{2})-(0?[13456789]|1[012])-(0?[1-9]|[12]\\\\d|30))|(((1[6-9]|[2-9]\\\\d)\\\\d{2})-0?2-(0?[1-9]|1\\\\d|2[0-8]))|(((1[6-9]|[2-9]\\\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))-0?2-29-)) (20|21|22|23|[0-1]?\\\\d):[0-5]?\\\\d:[0-5]?\\\\d$");
+        put("INT_RegExpr","^\\\\d{1,6}$");
+        put("KeyWord_RegExpr","^\\\\$");
     }};
 
     /**
      * value 值
-     * RegExpr 正则表达式类型(0/1/2/3)
+     * RegExpr 正则表达式类型(string)
      * minLength 匹配最短长度
      * maxLength 匹配最大长度
      * return 0-不合法 1-合法
