0a1dc0ab861128b3c5aa011b3295ce35c31a4b2e
D0ub1ePieR <535447508@qq.com>
2018-07-24 20:58:15 +0800

    add 正则匹配结果控制台展示 | fix 正则匹配表达式转义问题

--- src/main/java/cn/nuaa/spicydick/server/RegExprFix.java
+++ src/main/java/cn/nuaa/spicydick/server/RegExprFix.java
@@ -10,12 +10,12 @@ public class RegExprFix
     private static final HashMap <String, String> RegExprMap = new HashMap<String, String>()
     {{
         //地址匹配 (http(s))
-        put("URL_RegExpr","^http[s]?:\\\\/\\\\/([\\\\w-]+\\\\.)+[\\\\w-]+([\\\\w-./?%&=]*)?$");
+        put("URL_RegExpr","^http[s]?:\\/\\/([\\w-]+\\.)+[\\w-]+([\\w-./?%&=]*)?$");
         //mac地址匹配
         put("MAC_RegExpr","^([0-9A-Fa-f]{2}-){5}[0-9A-Fa-f]{2}$");
-        put("DATA_RegExpr","^((((1[6-9]|[2-9]\\\\d)\\\\d{2})-(0?[13578]|1[02])-(0?[1-9]|[12]\\\\d|3[01]))|(((1[6-9]|[2-9]\\\\d)\\\\d{2})-(0?[13456789]|1[012])-(0?[1-9]|[12]\\\\d|30))|(((1[6-9]|[2-9]\\\\d)\\\\d{2})-0?2-(0?[1-9]|1\\\\d|2[0-8]))|(((1[6-9]|[2-9]\\\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))-0?2-29-)) (20|21|22|23|[0-1]?\\\\d):[0-5]?\\\\d:[0-5]?\\\\d$");
-        put("INT_RegExpr","^\\\\d{1,6}$");
-        put("KeyWord_RegExpr","^\\\\$");
+        put("DATE_RegExpr","^((((1[6-9]|[2-9]\\d)\\d{2})-(0?[13578]|1[02])-(0?[1-9]|[12]\\d|3[01]))|(((1[6-9]|[2-9]\\d)\\d{2})-(0?[13456789]|1[012])-(0?[1-9]|[12]\\d|30))|(((1[6-9]|[2-9]\\d)\\d{2})-0?2-(0?[1-9]|1\\d|2[0-8]))|(((1[6-9]|[2-9]\\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))-0?2-29-)) (20|21|22|23|[0-1]?\\d):[0-5]?\\d:[0-5]?\\d$");
+        put("INT_RegExpr","^\\d{1,6}$");
+        put("KeyWord_RegExpr","^\\$");
     }};
 
     /**
@@ -38,7 +38,7 @@ public class RegExprFix
         }
         if (value.length()<maxLength && value.length()>minLength && Pattern.matches(RegExpr, value))
             res = true;
-        System.out.println("Res="+res);
+        System.out.println("限制长度的正则匹配 Res="+res);
         return res;
     }
     //不限制长度的正则匹配
@@ -54,7 +54,7 @@ public class RegExprFix
         }
         if (Pattern.matches(RegExpr, value))
             res=true;
-        System.out.println("Res="+res);
+        System.out.println("不限制长度的正则匹配 Res="+res);
         return res;
     }
     //检验长度
@@ -62,11 +62,13 @@ public class RegExprFix
     {
         if (value==null)
             return false;
+        System.out.println(value+" 检验长度 res="+(value.length()<=maxLength && value.length()>=minLength));
         return (value.length()<=maxLength && value.length()>=minLength);
     }
     //检验数值范围
     public static boolean formDetect(int value, int minvalue, int maxvalue)
     {
+        System.out.println(value+"数值检验范围res="+(value<=maxvalue && value>=minvalue));
         return (value<=maxvalue && value>=minvalue);
     }
 }
