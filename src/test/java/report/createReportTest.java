45085f08249a54d8a9e0710926f49e3675fbaaa7
D0ub1ePieR <535447508@qq.com>
2018-07-28 00:07:08 +0800

    add 添加报表各功能模块的测试类

---
+++ src/test/java/report/createReportTest.java
@@ -0,0 +1,52 @@
+package report;
+
+import cn.nuaa.spicydick.server.util.IniUtil;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
+
+/**
+ * {
+ * "version": "1.0",
+ * "method": "report.createReport",
+ * "params": null,
+ * "token": "#"
+ * "id":"#"
+ * }
+ */
+
+public class createReportTest {
+    public static void main(final String[] args){
+        IniUtil.changeMode(IniUtil.LOCAL_DEBUG_MODE);
+
+        //创建vertx实例，建立虚拟客户端
+        Vertx vertx = Vertx.vertx();
+        WebClient client = WebClient.create(vertx);
+
+        //创建报文json
+        JsonObject request = new JsonObject();
+        request.put("version", 1.0);
+        request.put("method", "report.createReport");
+        request.put("token", "");
+        request.put("id", 1);
+
+        client
+                .post(IniUtil.getInstance().getHTTPServerPort(), IniUtil.getInstance().getServerHostName(), "/")
+                .sendJsonObject(request, ar -> {
+                    if (ar.succeeded()) {
+
+                        HttpResponse<Buffer> response = ar.result();
+
+                        // Decode the body as a json object
+                        JsonObject body = response.bodyAsJsonObject();
+
+                        System.out.println("Received response with status code: " + response.statusCode() + " with body " + body);
+                    } else {
+                        System.out.println("Something went wrong " + ar.cause().getMessage());
+                    }
+                    vertx.close();
+                });
+    }
+}
---
+++ src/test/java/report/deleteReportTest.java
@@ -0,0 +1,58 @@
+package report;
+
+import cn.nuaa.spicydick.server.util.IniUtil;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
+
+/**
+ * {
+ * "version": "1.0",
+ * "method": "report.deleteReport",
+ * "params": null,
+ * "token":"#",
+ * "id":"#",
+ * }
+ */
+
+public class deleteReportTest {
+    public static void main(final String[] args){
+        IniUtil.changeMode(IniUtil.LOCAL_DEBUG_MODE);
+
+        //创建vertx实例，建立虚拟客户端
+        Vertx vertx = Vertx.vertx();
+        WebClient client = WebClient.create(vertx);
+
+        //创建虚拟参数
+        JsonObject params = new JsonObject();
+        params.put("reportId","1");
+
+        //创建报文json
+        JsonObject request = new JsonObject();
+        request.put("version", 1.0);
+        request.put("method", "report.deleteReport");
+        request.put("params",params);
+        request.put("token", "");
+        request.put("id", 1);
+
+        client
+                .post(IniUtil.getInstance().getHTTPServerPort(), IniUtil.getInstance().getServerHostName(), "/")
+                .sendJsonObject(request, ar -> {
+                    if (ar.succeeded()) {
+
+                        HttpResponse<Buffer> response = ar.result();
+
+                        // Decode the body as a json object
+                        JsonObject body = response.bodyAsJsonObject();
+
+                        System.out.println("Received response with status code: " + response.statusCode() + " with body " + body);
+                    } else {
+                        System.out.println("Something went wrong " + ar.cause().getMessage());
+                    }
+                    vertx.close();
+                });
+    }
+}
